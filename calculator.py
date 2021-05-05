aa29c4d4ca0a59359f2c6e0ae40438f07f10941a
Anirudh Bhat <48968640+bhatanirudh@users.noreply.github.com>
2021-05-05 18:01:00 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,30 @@
+def add(n1,n2):
+  return n1+n2 
+
+def sub(n1,n2):
+  return n1-n2
+
+def mul(n1,n2):
+  return n1*n2
+
+def div(n1,n2):
+  return n1/n2 
+flag=0
+while flag==0:
+
+ a=int(input("Enter first number"))
+ b=int(input("Enter second number"))
+ choice=input("Enter operation +, - , / , X ")
+ my_dict={
+  "+":add,
+  "-":sub,
+  "X":mul,
+  "/":div
+ }
+ for symbol in my_dict:
+   if choice==symbol:
+     function=my_dict[symbol]
+     print(function(a,b))
+ c=input("do you want to continue?y/n")
+ if c=="n":
+   flag=1 
